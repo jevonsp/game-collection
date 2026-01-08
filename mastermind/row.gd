2121d5a3da73b726894e47613f68c50c92040517
jevonsp <pineapplesituation@gmail.com>
2026-01-08 13:31:19 -0800

    Mastermind Start

---
+++ mastermind/row.gd
@@ -0,0 +1,11 @@
+extends HBoxContainer
+
+@onready var guesses_0: ColorRect = $Guesses/ColorRect0
+@onready var guesses_1: ColorRect = $Guesses/ColorRect1
+@onready var guesses_2: ColorRect = $Guesses/ColorRect2
+@onready var guesses_3: ColorRect = $Guesses/ColorRect3
+
+@onready var results_0: ColorRect = $Results/ColorRect0
+@onready var results_1: ColorRect = $Results/ColorRect1
+@onready var results_2: ColorRect = $Results/ColorRect2
+@onready var results_3: ColorRect = $Results/ColorRect3
